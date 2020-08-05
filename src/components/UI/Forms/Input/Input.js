9e46bc797f97e1d65cb45151b1c81178df7bec77
vladimir_dabic <vdabic17@raf.rs>
2020-08-05 04:30:26 +0200

    first commit

---
+++ src/components/UI/Forms/Input/Input.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes, { element } from 'prop-types';
+import classes from './Input.css'
+
+const Input = props => {
+
+    let inputElement = null;
+    const inputClasses = [classes.InputElement];
+
+    if (!props.valid && props.shouldValidate && props.touched) {
+        inputClasses.push(classes.Invalid);
+    }
+
+    switch (props.elementType) {
+        case 'input':
+            inputElement = <input
+            onChange={props.changed}    
+            className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value} />
+            break;
+        case 'textarea':
+            inputElement = <textarea
+            onChange={props.changed}    
+            className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value} />
+            break;
+        case 'select':
+            inputElement = (
+                <select
+                onChange={props.changed}    
+                className={inputClasses.join(' ')}
+                    value={props.value}>
+                    {
+                        props.elementConfig.options.map(option => {
+                        return <option key={option.value} value={option.value}>{option.displayValue}</option>
+                        })
+                    }
+                </select>
+            );
+            break;
+        default:
+            inputElement = <input
+            onChange={props.changed}    
+            className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value} />
+            break;
+    }
+
+    return (
+        <div className={classes.Input}>
+            <label className={classes.Label}>{props.label}</label>
+            {inputElement}
+        </div>
+    );
+};
+
+Input.propTypes = {
+    label: PropTypes.string,
+    elementType: PropTypes.string.isRequired,
+    elementConfig: PropTypes.object,
+    value: PropTypes.string,
+    changed: PropTypes.func.isRequired,
+    valid: PropTypes.bool,
+    touched: PropTypes.bool
+};
+
+export default Input;
